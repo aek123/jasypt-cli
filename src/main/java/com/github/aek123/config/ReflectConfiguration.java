a1508e855a8bc0375e40dc2210820f89556b39f3
aek123 <ertankayir@gmail.com>
2022-04-29 12:29:59 +0300

    ReflectConfiguration, RandomIvGenerator create via reflection.

--- src/main/java/com/github/aek123/config/ReflectConfiguration.java
+++ src/main/java/com/github/aek123/config/ReflectConfiguration.java
@@ -3,22 +3,25 @@ package com.github.aek123.config;
 import org.jasypt.commons.CommonUtils;
 import org.jasypt.digest.StandardStringDigester;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.jasypt.iv.RandomIvGenerator;
+import org.jasypt.iv.IvGenerator;
 import org.jasypt.registry.AlgorithmRegistry;
 
 import java.util.Set;
 
 public class ReflectConfiguration {
 
-    private static final String password = "some-random-password";
-
-    public static void main(String[] args) {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) throws Exception {
         String text = "hello world";
+        String password = "some-random-password";
+        IvGenerator ivGenerator = (IvGenerator) Class.forName("org.jasypt.iv.RandomIvGenerator")
+                                                     .getConstructor()
+                                                     .newInstance();
         Set<String> algorithms = AlgorithmRegistry.getAllPBEAlgorithms();
         for (String algorithm : algorithms) {
-            String enc = enc(text, algorithm);
+            String enc = enc(text, password, algorithm, ivGenerator);
             System.out.println(enc);
-            String dec = dec(enc, algorithm);
+            String dec = dec(enc, password, algorithm, ivGenerator);
             System.out.println(dec);
             assert dec.equals(text);
         }
@@ -32,21 +35,21 @@ public class ReflectConfiguration {
         }
     }
 
-    private static String enc(String text, String algorithm) {
+    private static String enc(String text, String password, String algorithm, IvGenerator ivGenerator) {
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         encryptor.setPassword(password);
         encryptor.setAlgorithm(algorithm);
-        encryptor.setIvGenerator(new RandomIvGenerator());
+        encryptor.setIvGenerator(ivGenerator);
         encryptor.setStringOutputType(CommonUtils.STRING_OUTPUT_TYPE_BASE64);
         return encryptor.encrypt(text);
     }
 
-    private static String dec(String text, String algorithm) {
+    private static String dec(String text, String password, String algorithm, IvGenerator ivGenerator) {
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         encryptor.setPassword(password);
         encryptor.setAlgorithm(algorithm);
         encryptor.setStringOutputType(CommonUtils.STRING_OUTPUT_TYPE_BASE64);
-        encryptor.setIvGenerator(new RandomIvGenerator());
+        encryptor.setIvGenerator(ivGenerator);
         return encryptor.decrypt(text);
     }
 }
